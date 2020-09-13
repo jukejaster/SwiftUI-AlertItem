a2ac2cd209aebce29c7db991fffbff73fa82f6bc
Juke Jaster <justin@justncode.com>
2020-09-12 22:43:08 -0400

    Initial Commit

---
+++ alert-item/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alert-item
+//
+//  Created by Juke Jaster on 9/12/20.
+//  Copyright © 2020 Juke Jaster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
